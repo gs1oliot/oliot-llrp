0c09e281c93c42af51ebe290610d26d0f3e5e907
vehumet <shheo+github@resl.kaist.ac.kr>
2014-07-08 16:17:02 +0900

    Update and rename README to README.mk

---
+++ README.mk
@@ -0,0 +1,5 @@
+Please refer to the wiki page on the right :)
+
+You can also see the generated Doxygen document at following URL
+
+http://gs1oliot.github.io/oliot-llrp-doxygen/Doxygen_docs/html/index.html
